7db50bd17c717c24dbccad99f2bcf2741f1ba4f7
prashanth <prashanth@Prashanth-PC>
2023-01-18 23:40:50 +0530

    0.0.1: Message pushed success

--- msgOne/src/main/java/com/msgSev/actvMQ/consumer/Consumer1.java
+++ msgOne/src/main/java/com/msgSev/actvMQ/consumer/Consumer1.java
@@ -1,8 +1,6 @@
 package com.msgSev.actvMQ.consumer;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,12 +8,14 @@ public class Consumer1 extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		Logger log = LoggerFactory.getLogger(Consumer1.class);
-		log.info("args before: " + "args");
-		// TODO Auto-generated method stub
+		
 //		from("activemq:myMq")
 //		.to("log:myLog");
-		log.info("args After: " + "args");
+		from("timer:active-mq-timer?period=60000")
+		.transform().constant("Hello Message from Apache Camel ")
+        .log("${body}")
+        //send this message to ActiveMQ queue
+        .to("activemq:my-activemq-queue");
 	}
 
 }
